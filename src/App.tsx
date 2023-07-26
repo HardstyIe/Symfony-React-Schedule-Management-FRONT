bdb677aa0ddaa23414949f48963e40e067e31756
HardstyIe <hardstyle.dd@gmail.com>
2023-07-26 11:35:07 +0200

    refactor(folder): refactor folder structure

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { HelmetProvider } from 'react-helmet-async';
+import Sidebar from './Components/Sidebar';
+
+export default function App() {
+  return (
+    <HelmetProvider>
+      <Sidebar />
+    </HelmetProvider>
+  );
+}
