da973f7a3c6bd115d682e9507e62b7bf073cd5ec
Poteau Florian <florianpoteau59@gmail.com>
2023-08-10 12:23:17 +0200

    feat(ressourcecours): container for the files pdf

--- src/Pages/RessourceCours.tsx
+++ src/Pages/RessourceCours.tsx
@@ -9,7 +9,14 @@ const RessourceCours = () => {
             <div className="flex">
                 <Sidebar />
                 <div className="flex-1 h-screen p-7 ml-8">
-                    <h1 className='text-lg pt-2'>Bienvenue sur la page Absences + {id}</h1>
+                    <h1 className='text-lg pt-2'>Ressource pédagogique du cours</h1>
+
+                    <div className="bg-white p-8 rounded shadow-md max-w-md ml-10 mt-20">
+                        <h1 className="text-2xl font-semibold mb-4">Voir ressource pédagogique</h1>
+                        <a href="/chemin/vers/votre/fichier.pdf" target="_blank" className="block bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded text-center">
+                            Ouvrir le fichier
+                        </a>
+                    </div>
                 </div>
             </div>
         </>
