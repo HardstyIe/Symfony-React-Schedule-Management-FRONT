5cf5857b3ecbc709bda7ad005bda1fdc337b6b18
Poteau Florian <florianpoteau59@gmail.com>
2023-08-14 14:15:34 +0200

    refactor(planning): new calendar who used to changed or add new schedules

--- src/Components/CalendarPlanning.tsx
+++ src/Components/CalendarPlanning.tsx
@@ -1,57 +1,64 @@
 import React, { useEffect, useRef } from 'react';
-import FullCalendar from '@fullcalendar/react';
+import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 interface ClassEvent {
+    id: string;
     title: string;
     start: Date;
     end: Date;
 }
 
 const CalendarPlanning: React.FC = () => {
-    const calendarRef = useRef<FullCalendar | null>(null);
+    const calendarRef = useRef<HTMLDivElement | null>(null);
+    let calendar: Calendar | null = null;
 
     useEffect(() => {
         if (calendarRef.current) {
-            const calendarApi = calendarRef.current.getApi();
-            calendarApi.gotoDate(new Date());
-            const events: ClassEvent[] = [
-                { title: '9H00 - 17H00', start: new Date('2023-08-03T09:00:00'), end: new Date('2023-08-03T17:00:00') },
-                { title: '9H00 - 17H00', start: new Date('2023-08-02T09:00:00'), end: new Date('2023-08-02T17:00:00') }
-                // ... Ajoutez d'autres événements pour d'autres classes et horaires
-            ];
-            calendarApi.addEventSource(events);
+            calendar = new Calendar(calendarRef.current, {
+                plugins: [dayGridPlugin, interactionPlugin],
+                weekends: false,
+                selectable: true,
+                events: [
+                    { id: 'event1', title: '9H00 - 17H00', start: '2023-08-03T09:00:00', end: '2023-08-03T17:00:00' },
+                    { id: 'event2', title: '9H00 - 17H00', start: '2023-08-02T09:00:00', end: '2023-08-02T17:00:00' }
+                    // ... Ajoutez d'autres événements pour d'autres classes et horaires
+                ],
+                select: function (info) {
+                    const enteredTitle = prompt('Entrez le(s) nouveau(x) horaire(s)');
+                    if (enteredTitle) {
+                        const newEvent: ClassEvent = {
+                            id: String(new Date().getTime()),
+                            title: enteredTitle,
+                            start: info.start,
+                            end: info.end
+                        };
+                        calendar.addEvent(newEvent);
+                    }
+                    calendar.unselect();
+                },
+                eventClick: function (info) {
+                    const newTitle = prompt('Modifiez le(s) horaire(s):', info.event.title);
+                    if (newTitle) {
+                        const updatedEvent: ClassEvent = {
+                            ...info.event.extendedProps,
+                            title: newTitle,
+                            start: info.event.start,
+                            end: info.event.end
+                        };
+                        info.event.remove();
+                        calendar.addEvent(updatedEvent);
+                    }
+                }
+            });
+
+            calendar.render();
         }
     }, []);
 
     return (
-        <div>
-            <FullCalendar
-                plugins={[dayGridPlugin]}
-                initialView="dayGridMonth"
-                ref={calendarRef}
-                weekends={false}
-
-                eventContent={(info) => {
-                    return (
-                        <div className="flex flex-col items-center">
-                            <span className="mb-2 text-center">{info.event.title}</span>
-                            <div className="mx-auto">
-                                <button
-                                    onClick={(e) => {
-                                        e.stopPropagation();
-                                        console.log('Modifier', info.event);
-                                    }}
-                                    className="px-2 py-1 bg-blue-500 text-white rounded-md"
-                                >
-                                    Modifier
-                                </button>
-                            </div>
-                        </div>
-                    );
-                }}
-            />
-        </div>
+        <div ref={calendarRef}></div>
     );
 };
 
--- src/Pages/Planning.tsx
+++ src/Pages/Planning.tsx
@@ -11,8 +11,8 @@ const Planning = () => {
                 <Sidebar />
 
                 {/* div contenu coté droit */}
-                <div className="flex-1 h-screen p-7 ml-8">
-                    <h1 className='text-lg pt-2'>Bienvenue sur la page planning</h1>
+                <div className="flex-1 h-screen p-0 pt-7 md:p-7 ml-8">
+                    <h1 className='text-lg pt-2 mb-10 md:mb-0'>Bienvenue sur la page planning</h1>
                     <CalendarPlanning />
                 </div>
             </div>
