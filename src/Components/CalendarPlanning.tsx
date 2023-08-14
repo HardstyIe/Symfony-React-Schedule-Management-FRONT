e229a13481e1be052cb17b365ece80be87acec89
Poteau Florian <florianpoteau59@gmail.com>
2023-08-14 15:16:53 +0200

    refactor(calendar planning): add locale fr, displayEventTime: false and delete schedules with alert

--- src/Components/CalendarPlanning.tsx
+++ src/Components/CalendarPlanning.tsx
@@ -20,11 +20,16 @@ const CalendarPlanning: React.FC = () => {
                 plugins: [dayGridPlugin, interactionPlugin],
                 weekends: false,
                 selectable: true,
+                locale: 'fr',
+                displayEventTime: false,
                 events: [
                     { id: 'event1', title: '9H00 - 17H00', start: '2023-08-03T09:00:00', end: '2023-08-03T17:00:00' },
                     { id: 'event2', title: '9H00 - 17H00', start: '2023-08-02T09:00:00', end: '2023-08-02T17:00:00' }
                     // ... Ajoutez d'autres événements pour d'autres classes et horaires
                 ],
+
+                // Fonction qui permet de selectionner une ou plusieurs dates pour les modifier
+                // Cette fonction doit etre disponible que pour les roles administrateurs
                 select: function (info) {
                     const enteredTitle = prompt('Entrez le(s) nouveau(x) horaire(s)');
                     if (enteredTitle) {
@@ -38,19 +43,30 @@ const CalendarPlanning: React.FC = () => {
                     }
                     calendar.unselect();
                 },
+
+                // Cette fonction permet de modifier une horaire existante en cliquant dessus
+                // Cette fonction doit etre disponible que pour les roles administrateurs
                 eventClick: function (info) {
-                    const newTitle = prompt('Modifiez le(s) horaire(s):', info.event.title);
-                    if (newTitle) {
-                        const updatedEvent: ClassEvent = {
-                            ...info.event.extendedProps,
-                            title: newTitle,
-                            start: info.event.start,
-                            end: info.event.end
-                        };
-                        info.event.remove();
-                        calendar.addEvent(updatedEvent);
+                    const newTitle = prompt("Modifiez le(s) horaire(s) ou tapez supprimer pour supprimer l'horaire:", info.event.title);
+
+                    if (newTitle !== null) {
+                        if (newTitle.toLowerCase() === 'supprimer') {
+                            info.event.remove();
+                        } else {
+                            const updatedEvent: ClassEvent = {
+                                ...info.event.extendedProps,
+                                title: newTitle,
+                                start: info.event.start,
+                                end: info.event.end
+                            };
+                            info.event.remove();
+                            calendar.addEvent(updatedEvent);
+                        }
                     }
                 }
+
+
+
             });
 
             calendar.render();
